0417d4fcecbbca0edbe7fdf78e007ae589ba8f6f
david-schr <david.schrenk@protonmail.com>
2022-12-14 16:49:16 +0100

    Enable live subprocess monitoring

--- monitor.py
+++ monitor.py
@@ -1,7 +1,9 @@
 import shlex
 import subprocess
 import threading
+import selectors
 from time import sleep
+from typing import Callable
 
 from monitor import tcp_proxy
 import logger_factory
@@ -18,6 +20,58 @@ target_port = 12345
 main_logger = logger_factory.construct_logger("monitor")
 subprocess_logger = logger_factory.construct_logger("subprocess")
 
+
+def _monitor_process_output(process_handle: subprocess.Popen,
+                            stdout_handler: Callable[[bytes], bytes] = None,
+                            stderr_handler: Callable[[bytes], bytes] = None,
+                            monitor_frequency_secs: float = 0.1,
+                            monitor_timeout_secs: float = 5,
+                            max_timeout_until_io_is_ready_secs: float = 10) -> tuple[bytes, bytes]:
+    stdout_buffer = b""
+    stderr_buffer = b""
+
+    # Register IO event handler on stdout and stderr buffers
+    sel = selectors.DefaultSelector()
+    sel.register(process_handle.stdout, selectors.EVENT_READ)
+    sel.register(process_handle.stderr, selectors.EVENT_READ)
+
+    # End the loop if we have slept 'no_data_counter' times for a time of 'monitor_frequency_secs'
+    no_data_counter = 0
+    while no_data_counter < (monitor_timeout_secs / monitor_frequency_secs):
+        has_data = False
+        for key, _ in sel.select(timeout=max_timeout_until_io_is_ready_secs):
+            data = key.fileobj.read1()
+            if not data:
+                break
+
+            # Pass data to the relevant handler and add the result to the respective buffer
+            has_data = True
+            if key.fileobj is process_handle.stdout:
+                if stdout_handler is None:
+                    stdout_buffer += data
+                else:
+                    stdout_buffer += stdout_handler(data)
+            else:
+                if stderr_handler is None:
+                    stderr_buffer += data
+                else:
+                    stderr_handler(data)
+
+        # If no data was read in this iteration we increase the no_data_counter and sleep
+        if has_data:
+            no_data_counter = 0
+        else:
+            no_data_counter += 1
+            # We also stop if the process has stopped
+            if process_handle.poll():
+                subprocess_logger.debug("Subprocess ended")
+                break
+
+            sleep(monitor_frequency_secs)
+
+    return stdout_buffer, stderr_buffer
+
+
 if __name__ == "__main__":
     # Spawn the proxy socket
     function = tcp_proxy.start_listening
@@ -26,12 +80,14 @@ if __name__ == "__main__":
     socket_thread.start()
 
     # Run the subprocess which should be monitored
-    sleep(1)
     command_line = f"bash ./send.sh {target_address} {target_port}"
     main_logger.debug("Starting subprocess: " + command_line)
-    process = subprocess.run(shlex.split(command_line), capture_output=True)
-    subprocess_logger.debug(f"STDOUT: {process.stdout}")
-    subprocess_logger.debug(f"STDERR: {process.stderr}")
+
+    process = subprocess.Popen(shlex.split(command_line), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    stdout_buffer, stderr_buffer = _monitor_process_output(process, None, None)
+
+    subprocess_logger.debug(f"STDOUT: {stdout_buffer}")
+    subprocess_logger.debug(f"STDERR: {stderr_buffer}")
     subprocess_logger.debug(f"RETURN: {process.returncode}")
 
     main_logger.debug("Subprocess finished")
--- send.sh
+++ send.sh
@@ -1,6 +1,15 @@
 #!/usr/bin/env bash
 
+
+
 exec 3<>/dev/tcp/$1/$2
-echo -e "TEST" >&3
-#cat <&3
+for i in {1..9}
+do
+  echo -e "$i" >&3
+
+  echo "$i"
+  echo "$i" >&2
+done
+# cat <&3
 echo "Finished sending TEST"
+exit 1
